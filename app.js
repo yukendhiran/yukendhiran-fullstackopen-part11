4606c4d22b76df618d10c1471618f27a07dc5b2c
yukendhiran <yukendhiran4@gmail.com>
2024-08-31 08:30:10 +0530

    Fix Broken Health Check

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
 
